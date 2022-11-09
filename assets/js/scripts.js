f334b45370b913f6395cb35fa4b1b62474a1f0a8
thaispaes <thaisreginapaesdossantos@gmail.com>
2022-11-08 22:53:04 -0300

    feat: projeto finalizado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,51 +1,56 @@
-// const logo = document.createElement('img');
-// logo.src = './assets/img/logo.png';
+const logo = document.createElement('img');
+logo["src"] = './assets/img/logo-anime.jpg';
+const app = document.getElementById('root');
 
-// const container = document.createElement('div');
-// container.setAttribute('class', 'container');
+const title = document.createElement('h2');
+title.setAttribute('class', 'header');
+title.textContent = 'Anime Trending';
 
-// app.appendChild(logo);
-// app.appendChild(container);
+const paragraph = document.createElement('p');
+paragraph.setAttribute('class', 'p-header');
+paragraph.textContent = 'List of trending animes most updated';
+
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(title);
+app.appendChild(paragraph);
+app.appendChild(container);
+
 
 
 
 var request = new XMLHttpRequest();
-var result = fetch('https://kitsu.io/api/edge/anime')
-.then(data => {
-return data.json();
-});
-console.log(result[0])
-request.open('GET', 'https://kitsu.io/api/edge/anime', true);
+request.open('GET', 'https://kitsu.io/api/edge/trending/anime', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
-  const data = JSON.parse(this.response);
-  console.log(data)
+  const animeAsJson = JSON.parse(this.response);
+  console.log(animeAsJson.data[0])
   if (request.status >= 200 && request.status < 400) {
         var result = {}
 
-        for (var i in data)
+        for (let i = 0; i < animeAsJson.data.length ;i++)
         {
-            console.log(data.data[i])
+            const card = document.createElement('div');
+            card.setAttribute('class', 'card');
+
+            const h1 = document.createElement('h1');
+            h1.textContent = animeAsJson.data[i].attributes.canonicalTitle;
+
+            
+
+            const p = document.createElement('p');
+            p.textContent = animeAsJson.data[i].attributes.description;
+
+            container.appendChild(card);
+            card.appendChild(h1);
+            card.appendChild(p);
+
         }
-
-        console.log(result)
-
-        data.forEach(anime => {
-        const card = document.createElement('div');
-        card.setAttribute('class', 'card');
-
-        const h1 = document.createElement('h1');
-        h1.textContent = anime.titles;
-
-        const p = document.createElement('p');
-        anime.synopsis = anime.synopsis.substring(0, 300);
-        p.textContent = ""+anime.synopsis;
-
-        container.appendChild(card);
-        card.appendChild(h1);
-        card.appendChild(p);
-        });
+        
     } 
     else {
         const errorMessage = document.createElement('marquee');
